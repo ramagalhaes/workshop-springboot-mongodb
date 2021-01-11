83cad02f57a86da3f7074dbe0cb193be9a2fdc0c
raphael magalhaes <raphael.gol@hotmail.com>
2021-01-11 19:09:36 -0300

    Project created

---
+++ src/main/java/com/workshop/Wokshopmongo1Application.java
@@ -0,0 +1,13 @@
+package com.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wokshopmongo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wokshopmongo1Application.class, args);
+	}
+
+}
